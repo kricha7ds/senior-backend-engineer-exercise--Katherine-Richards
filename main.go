76026fc8257c4f01e5a24cfbb4aaacf807f68f55
Katherine Richards <kricha7ds@gmail.com>
2024-10-31 16:17:25 -0600

    Add db connection

--- main.go
+++ main.go
@@ -3,4 +3,33 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
+
+var DB *gorm.DB
+
+func dbConnect() {
+	db, err := gorm.Open(sqlite.Open("employees.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database.")
+	}
+
+	DB = db
+}
+
+func home(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"success": "success",
+	})
+}
+
+func main() {
+	dbConnect()
+
+	router := gin.Default()
+	router.GET("/", home)
+
+	router.Run(":8080")
+}
